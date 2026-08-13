cb150be2bf49e77f7261327acd8f7abace0a80d5
MariammlM-2003 <jothijothi37790@gmail.com>
2026-08-13 17:49:20 +0530

    fresh_niningo

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 
+from .views import ProfileView
+
+
 urlpatterns = [
+    path(
+        "profile/",
+        ProfileView.as_view(),
+        name="profile"
+    ),
 ]
