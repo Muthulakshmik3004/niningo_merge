b4bba84bdfcaaf9079dfaa59438ae9f52bb2cb40
MariammlM-2003 <jothijothi37790@gmail.com>
2026-08-13 16:09:05 +0530

    fresh_niningo

--- backend/niningo/settings.py
+++ backend/niningo/settings.py
@@ -9,11 +9,24 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+import mongoengine
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+
+mongoengine.connect(
+    db=MONGO_DB_NAME,
+    host=MONGO_URI,
+)
 
 
 # Quick-start development settings - unsuitable for production
